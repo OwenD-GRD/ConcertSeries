6bd15d56c6866f9db1e94d02d9f83f4f1de3cdf8
OwenD-GRD <workowend@gmail.com>
2023-05-25 11:53:16 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,43 @@
+<footer class="container-fluid text-center" id="page_footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">    
+                </div> <!--col-md-4-->
+                <div class="col-md-4">
+                    <img class="img-fluid social-icons" alt="Facebook Icon" src="./Images/Facebook-Icon.jpg">
+                    <img class="img-fluid social-icons" alt="Instagram Icon" src="./Images/Instagram-Icon.jpg">  
+                    <img class="img-fluid social-icons" alt="Twitter Icon" src="./Images/Twitter-Icon.jpg">  
+                </div> <!--col-md-4-->
+                <div class="col-md-4">    
+                </div> <!--col-md-4-->
+            </div> <!--row-->
+
+            <div class="row">
+                <div class="col-md-2">
+                </div> <!--col-md-2-->
+                <div class="col-md-8 blocker">
+                </div> <!--col-md-8-->
+                <div class="col-md-2">
+                </div> <!--col-md-2-->
+            </div> <!--row-->
+
+            <div class="row">
+                <div class="col-md-3">
+                </div>
+                <div class="col-md-3">
+                   <p class="text-align-center"><strong>Location:</strong> 1 Finnerty Street Fremantle Western Australia</p> 
+                </div> <!--col-md-6-->
+                <div class="col-md-3">
+                    <p class="text-align-center"><strong>Contact:</strong> +61 8 9432 9555</p>
+                </div> <!--col-md-6-->
+                <div class="col-md-3">
+                </div>
+            </div> <!--row-->
+        </div> <!--container-->
+    </footer> <!--container-fluid-->
+    <?php wp_footer(); ?>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script> -->
+</body>
+</html>
