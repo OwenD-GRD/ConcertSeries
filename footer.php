95ed1aa8a62cade7460a44e069a2d7312d498618
OwenD-GRD <workowend@gmail.com>
2023-06-01 14:25:37 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
-<footer class="container-fluid text-center" id="page_footer">
+<footer class="container-fluid text-center" id="page_footer" style="background-size:cover; padding-top: 4%; padding-bottom: 4%; max-width: 100%; height: auto; background-image: url(<?php the_field('footerbackgroundimage') ?>);">
         <div class="container">
             <div class="row">
                 <div class="col-md-4">    
                 </div> <!--col-md-4-->
                 <div class="col-md-4">
-                    <img class="img-fluid social-icons" alt="Facebook Icon" src="http://170.187.231.66/~mesh6/wp-content/themes/ConcertSeries/Images/Facebook-Icon.jpg">
-                    <img class="img-fluid social-icons" alt="Instagram Icon" src="http://170.187.231.66/~mesh6/wp-content/themes/ConcertSeries/Images/Instagram-Icon.jpg">  
-                    <img class="img-fluid social-icons" alt="Twitter Icon" src="http://170.187.231.66/~mesh6/wp-content/themes/ConcertSeries/Images/Twitter-Icon.jpg">  
+                    <img class="img-fluid social-icons" alt="Facebook Icon" src="<?php the_field('footericonone'); ?>">
+                    <img class="img-fluid social-icons" alt="Instagram Icon" src="<?php the_field('footericontwo'); ?>">  
+                    <img class="img-fluid social-icons" alt="Twitter Icon" src="<?php the_field('footericonthree'); ?>">  
                 </div> <!--col-md-4-->
                 <div class="col-md-4">    
                 </div> <!--col-md-4-->
@@ -25,10 +25,10 @@
                 <div class="col-md-3">
                 </div>
                 <div class="col-md-3">
-                   <p class="text-align-center"><strong>Location:</strong> 1 Finnerty Street Fremantle Western Australia</p> 
+                   <p class="text-align-center"><?php the_field('location'); ?></p> 
                 </div> <!--col-md-6-->
                 <div class="col-md-3">
-                    <p class="text-align-center"><strong>Contact:</strong> +61 8 9432 9555</p>
+                    <p class="text-align-center"><?php the_field('contactdetails'); ?></p>
                 </div> <!--col-md-6-->
                 <div class="col-md-3">
                 </div>
